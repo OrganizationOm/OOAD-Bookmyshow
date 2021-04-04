a5bbef889efc4b06996addac1705996e21ed582d
Om Shreenidhi <om_719@yahoo.com>
2021-04-04 19:49:29 +0530

    Basic UI and Functionality

--- app.py
+++ app.py
@@ -15,15 +15,54 @@ def home():
 
 @app.route("/about_movie.html",methods=["POST","GET"])
 def about_movie():
-    return render_template("about_movie.html")
+    movie_data={}
+    if request.method == "POST":
+        movie_data=request.form['movie']
+        f = open('movie_file.json')
+        data=json.load(f)
+        movie_data=data[movie_data]
+    return render_template("about_movie.html", content = movie_data)
 
 @app.route("/theatre.html",methods=["POST","GET"])
 def theatre():
-    return render_template("theatre.html")
+    movie_data={}
+    theatre_data={}
+    if request.method == "POST":
+        movie_data=request.form['movie']
+        f = open('movie_file.json')
+        data=json.load(f)
+        movie_data=data[movie_data]
+        f = open('theatre_file.json')
+        theatre_data=json.load(f)
+        # print(theatre_data["Inox Multiplex"])
+
+    return render_template("theatre.html", content = movie_data['name'] , theatres=theatre_data)
 
 @app.route("/ticket_booking.html",methods=["POST","GET"])
 def ticket_booking():
-    return render_template("ticket_booking.html")
+    movie_data=""
+    theatre_name=""
+    movie_time=""
+    message=""
+    if request.method == "POST":
+        movie_data=request.form['movie']
+        theatre_name=request.form['theatre_name']
+        movie_time=request.form['movie_time']
+        quantity=request.form['quantity']
+        f = open('movie_file.json')
+        data=json.load(f)
+        movie_data=data[movie_data]
+        if(quantity=="0"):
+            message=""
+        else:
+            message="You have successfully booked "+quantity+" seats for the movie "+movie_data["name"]+" at "+movie_time+", "+theatre_name
+        print("this is the "+quantity)
+        print(movie_data)
+        print(theatre_name)
+        print(movie_time)
+
+    
+    return render_template("ticket_booking.html", content=movie_data, theatre_name=theatre_name, movie_time=movie_time, msg=message)
 
 
 if __name__=="__main__":
