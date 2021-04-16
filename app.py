1c6ed2a3c82b6962d9b5a6ac8af891e956eb2b57
Om Shreenidhi <om_719@yahoo.com>
2021-04-16 22:16:05 +0530

    Add and delete movies

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask,render_template,url_for,redirect,request,session
+from werkzeug.utils import secure_filename
 import json
+import os
+UPLOAD_FOLDER = 'static/imgs'
 
 app=Flask(__name__)
 app.secret_key="abc"
-
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 class Movie_class:
     def __init__(self, name="", rating="",description="",image=""):
@@ -62,6 +65,8 @@ class User_class:
             return False
         if(user_data[username]["password"]==password):
             return True
+        else:
+            return False
 
     def register_user(self,username,password,email,phone_no):
         f = open('user_file.json')
@@ -94,6 +99,73 @@ class User_class:
             user_data_history[session["username"]].append(movie_name)
         with open("user_history_file.json","w") as write_file:
             json.dump(user_data_history,write_file)
+        
+
+
+
+
+class Admin_Class:
+
+    def __init__(self, username="", password=""):
+        self.username=username
+        self.password=password
+    
+    def add_admin_user(self, username,password,email,phone):
+        f = open('admin_file.json')
+        admin_data=json.load(f)
+        if(username in admin_data):
+            return False
+        admin_data[username]={"username":username,"password":password,"email":email,"phone_no":phone}   
+        with open("admin_file.json", "w") as write_file:
+            json.dump(admin_data, write_file)
+        return True
+
+    def validate_login_admin(self, username, password):
+        f = open('admin_file.json')
+        admin_data=json.load(f)
+        if(username not in admin_data):
+            return False
+        if(admin_data[username]["password"]==password):
+            return True
+        else :
+            return False
+
+    def check_admin_login(self): 
+        if "username" in session:
+            return True
+        else:
+            return False
+
+
+    def logout_admin_user(self):
+        session.pop('username',None)
+
+    
+    def add_movie(self,movie_name,rating,description,file):
+        f=open("movie_file.json")
+        movie_data=json.load(f)
+        if movie_name in movie_data:
+            return False
+        movie_data[movie_name]={"name":movie_name,"rating":rating,"description":description,"image":file.filename}
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        with open("movie_file.json", "w") as write_file:
+            json.dump(movie_data, write_file)
+        return True
+
+
+    def delete_movie(self,movie_name):
+        f=open("movie_file.json")
+        movie_data=json.load(f)
+        if movie_name not in movie_data:
+            return False
+        movie_data.pop(movie_name, None)
+        with open("movie_file.json", "w") as write_file:
+            json.dump(movie_data, write_file)
+        return True
+        
+        
+        
 
 
 
@@ -124,6 +196,8 @@ Things to be added:-
 def home():
     user_obj=User_class()
     user_obj.logout_user()
+    admin_obj=Admin_Class()
+    admin_obj.logout_admin_user()
     get_movies=Movie_class()
     movie_list=get_movies.movie_object_list()
     return render_template("movie_display.html", content = movie_list)
@@ -135,12 +209,22 @@ def movie_display_logged_in():
     return render_template("movie_display_logged_in.html", content = movie_list)
 
 
+@app.route("/movie_display_admin_logged_in.html",methods=["POST","GET"])
+def movie_display_admin_logged_in():
+    get_movies=Movie_class()
+    movie_list=get_movies.movie_object_list()
+    return render_template("movie_display_admin_logged_in.html", content = movie_list)
+
+
 @app.route("/about_movie.html",methods=["POST","GET"])
 def about_movie():
     movie_data={}
     if request.method == "POST":
         user_obj=User_class()
+        admin_obj=Admin_Class()
         if(user_obj.check_login()==False):
+            return redirect(url_for("home"))
+        elif(admin_obj.check_admin_login()==False):
             return redirect(url_for("home"))
         movie_name=request.form['movie']
         movie_info=Movie_class()
@@ -193,10 +277,15 @@ def login():
         username=request.form["username"]
         password=request.form["password"]
         user_obj=User_class()
+        admin_obj=Admin_Class()
         check=user_obj.validate_login(username,password)
+        check_admin=admin_obj.validate_login_admin(username,password)
         if(check):
             session["username"]=username
             return redirect(url_for("movie_display_logged_in"))
+        elif(check_admin):
+            session["username"]=username
+            return redirect(url_for('movie_display_admin_logged_in'))
         else:
             message="Invalid"
 
@@ -220,6 +309,31 @@ def register():
             message="Already registered"
 
     return render_template("register.html",msg=message)
+
+@app.route("/delete_movie",methods=["POST","GET"])
+def delete_movie():
+    movie_name=request.form["movie"]
+    #print(movie_name)
+    admin_obj=Admin_Class()
+    admin_obj.delete_movie(movie_name)
+    return redirect(url_for('movie_display_admin_logged_in'))
+
+@app.route("/add_movie.html",methods=["POST","GET"])
+def add_movie():
+    if request.method!="POST":
+        return render_template("add_movie.html")
+    movie_name=request.form["movie_name"]
+    rating=request.form["rating"]
+    description=request.form["description"]
+    # image=request.form["image"]
+    file = request.files['file']
+    print(file.filename)
+    admin_obj=Admin_Class()
+    check=admin_obj.add_movie(movie_name,rating,description,file)
+    if(check):
+        return redirect(url_for('movie_display_admin_logged_in'))
+    else:
+        return render_template("add_movie.html",msg="Movie already added")
 
 if __name__=="__main__":
 	app.run(debug = True)
--- temp.py
+++ temp.py
@@ -1,12 +1,14 @@
 import json
 
-# data={"Inox Multiplex" : ["10:00 am","12:00 pm", "2:00 pm", "4:00 pm" , "6:00 pm", "8:00 pm", "10:00 pm"], "PVR Whitefield": ["10:15 am","12:30 pm", "2:45 pm", "4:15 pm" , "6:30 pm", "8:35 pm", "10:40 pm"], "Fame Cinemas" : ["9:15 am","11:30 pm", "1:45 pm", "2:15 pm"], "VR Mall" : [ "6:30 pm", "8:35 pm", "10:40 pm"]}
-# f = open('movie_file.json')
-# data=json.load(f)
+data={"admin_Om": {"username": "admin_Om", "password": "hello123", "email": "omomomshreenidhi@gmail.com"}}
+# = open('movie_file.json')
+#data=json.load(f)
 
 # data["Avengers"]={"name":"Avengers","rating":"5","description":"After half of all life is snapped away by Thanos, the Avengers are left scattered and divided. Now with a way to reverse the damage, the Avengers and their allies must assemble once more and learn to put differences aside in order to work together and set things right. Along the way, the Avengers realize that sacrifices must be made as they prepare for the ultimate final showdown with Thanos, which will result in the heroes fighting the biggest battle they have ever faced."}
 
 
-data= {"Om719" : []}
-with open("user_history_file.json", "w") as write_file:
+#{ "Black Panther": { "name": "Black Panther", "rating": "4", "description": "Follows T'Challa who, after the death of his father, the King who of Wakanda, returns home to the isolated, technologically advanced African nation to succeed to the throne and take his rightful place as king. But when a powerful old enemy reappears, T'Challa's mettle as king and Black Panther is tested when he is drawn into a formidable conflict that puts the fate of Wakanda and the entire world at risk. Faced with treachery and danger, the young king must rally his allies and release the full power of Black Panther to defeat his foes and secure the safety of his people and their way of life.", "image": "black_panther.png" }, "Avengers": { "name": "Avengers", "rating": "5", "description": "After half of all life is snapped away by Thanos, the Avengers are left scattered and divided. Now with a way to reverse the damage, the Avengers and their allies must assemble once more and learn to put differences aside in order to work together and set things right. Along the way, the Avengers realize that sacrifices must be made as they prepare for the ultimate final showdown with Thanos, which will result in the heroes fighting the biggest battle they have ever faced.", "image": "avengers.png" } }
+
+#data= {"Om719" : []}
+with open("admin_file.json", "w") as write_file:
     json.dump(data, write_file)
